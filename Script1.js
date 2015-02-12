5923b94dd565bcd55f1234c976b42c1eeaa79b69
Barney Li <barney.li@outlook.com>
2015-02-12 22:10:40 +0800

    added external javascript, and updated html to include it

---
+++ Script1.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+function alterlist() {
+    document.getElementById("item1").innerHTML = "first item after js";
+    document.getElementById("item2").innerHTML = "second item after js";
+}
